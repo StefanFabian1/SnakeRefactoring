17963f7c8b51eb167f1eebafec8f40647fadaeab
Stefan Fabian <stefafabian@gmail.com>
2025-06-01 22:26:10 +0200

    init noveho projektu

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Snake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vitaj v hre Snake!");
+        }
+    }
+} 
