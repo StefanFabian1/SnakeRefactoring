f431a13424ff72f8742cd587553e2dd1e8260003
Stefan Fabian <stefafabian@gmail.com>
2025-06-01 22:46:17 +0200

    Nahradenie triedy pixel triedou Position, uprava povodneho kodu pre pouzivanie tried Position a Gameboard

--- Core/Position.cs
+++ Core/Position.cs
@@ -2,5 +2,13 @@ namespace Snake.Core
 {
     public class Position
     {
+        public int X { get; set; }
+        public int Y { get; set; }
+
+        public Position(int x = 0, int y = 0)
+        {
+            X = x;
+            Y = y;
+        }
     }
 } 
--- Program.cs
+++ Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Threading;
+using Snake.Game;
+using Snake.Core;
 ///█ ■
 ////https://www.youtube.com/watch?v=SGZgvMwjq2U
 namespace Snake
@@ -15,41 +17,39 @@ namespace Snake
             Random randomnummer = new Random();
             int score = 5;
             int gameover = 0;
-            pixel hoofd = new pixel();
-            hoofd.xpos = screenwidth/2;
-            hoofd.ypos = screenheight/2;
-            hoofd.schermkleur = ConsoleColor.Red;
+            // TODO: Inicializácia GameBoard
+            var gameBoard = new GameBoard();
+            Position hoofd = new Position(gameBoard.Width/2, gameBoard.Height/2);
+            ConsoleColor hoofdColor = ConsoleColor.Red;
             string movement = "RIGHT";
             List<int> xposlijf = new List<int>();
             List<int> yposlijf = new List<int>();
-            int berryx = randomnummer.Next(0, screenwidth);
-            int berryy = randomnummer.Next(0, screenheight);
+            int berryx = randomnummer.Next(0, gameBoard.Width);
+            int berryy = randomnummer.Next(0, gameBoard.Height);
             DateTime tijd = DateTime.Now;
             DateTime tijd2 = DateTime.Now;
             string buttonpressed = "no";
-            // TODO: Inicializácia GameBoard
-            var gameBoard = new GameBoard();
             while (true)
             {
                 Console.Clear();
-                if (hoofd.xpos == screenwidth-1 || hoofd.xpos == 0 ||hoofd.ypos == screenheight-1 || hoofd.ypos == 0)
+                if (hoofd.X == gameBoard.Width-1 || hoofd.X == 0 || hoofd.Y == gameBoard.Height-1 || hoofd.Y == 0)
                 { 
                     gameover = 1;
                 }
                 // TODO: Vykreslenie hraníc cez GameBoard
                 gameBoard.DrawBorders();
                 Console.ForegroundColor = ConsoleColor.Green;
-                if (berryx == hoofd.xpos && berryy == hoofd.ypos)
+                if (berryx == hoofd.X && berryy == hoofd.Y)
                 {
                     score++;
-                    berryx = randomnummer.Next(1, screenwidth-2);
-                    berryy = randomnummer.Next(1, screenheight-2);
+                    berryx = randomnummer.Next(1, gameBoard.Width-2);
+                    berryy = randomnummer.Next(1, gameBoard.Height-2);
                 } 
                 for (int i = 0; i < xposlijf.Count(); i++)
                 {
                     Console.SetCursorPosition(xposlijf[i], yposlijf[i]);
                     Console.Write("■");
-                    if (xposlijf[i] == hoofd.xpos && yposlijf[i] == hoofd.ypos)
+                    if (xposlijf[i] == hoofd.X && yposlijf[i] == hoofd.Y)
                     {
                         gameover = 1;
                     }
@@ -58,8 +58,8 @@ namespace Snake
                 {
                     break;
                 }
-                Console.SetCursorPosition(hoofd.xpos, hoofd.ypos);
-                Console.ForegroundColor = hoofd.schermkleur;
+                Console.SetCursorPosition(hoofd.X, hoofd.Y);
+                Console.ForegroundColor = hoofdColor;
                 Console.Write("■");
                 Console.SetCursorPosition(berryx, berryy);
                 Console.ForegroundColor = ConsoleColor.Cyan;
@@ -95,21 +95,21 @@ namespace Snake
                         }
                     }
                 }
-                xposlijf.Add(hoofd.xpos);
-                yposlijf.Add(hoofd.ypos);
+                xposlijf.Add(hoofd.X);
+                yposlijf.Add(hoofd.Y);
                 switch (movement)
                 {
                     case "UP":
-                        hoofd.ypos--;
+                        hoofd.Y--;
                         break;
                     case "DOWN":
-                        hoofd.ypos++;
+                        hoofd.Y++;
                         break;
                     case "LEFT":
-                        hoofd.xpos--;
+                        hoofd.X--;
                         break;
                     case "RIGHT":
-                        hoofd.xpos++;
+                        hoofd.X++;
                         break;
                 }
                 if (xposlijf.Count() > score)
@@ -118,15 +118,9 @@ namespace Snake
                     yposlijf.RemoveAt(0);
                 }
             }
-            Console.SetCursorPosition(screenwidth / 5, screenheight / 2);
+            Console.SetCursorPosition(gameBoard.Width / 5, gameBoard.Height / 2);
             Console.WriteLine("Game over, Score: "+ score);
-            Console.SetCursorPosition(screenwidth / 5, screenheight / 2 +1);
-        }
-        class pixel
-        {
-            public int xpos { get; set; }
-            public int ypos { get; set; }
-            public ConsoleColor schermkleur { get; set; }
+            Console.SetCursorPosition(gameBoard.Width / 5, gameBoard.Height / 2 +1);
         }
     }
 }
